c3e784152f78f0faad9226015c358f751845c877
ndevadoss <itznityadoss@gmail.com>
2017-06-19 14:05:06 -0500

    delete old folder

--- Rolling_Brooklyn.R
+++ Rolling_Brooklyn.R
@@ -1,1 +1,8 @@
-str(rollingsales_brooklyn)
+library(plyr)
+bk <- read.csv("C:/Nithya/MSDS/Term1/6306_Doing_DS/Week6/Live/Download/rollingsales_brooklyn.csv",skip=4,header=TRUE)
+bk
+str(bk)
+head(bk)
+summary(bk)
+str(bk)
+
